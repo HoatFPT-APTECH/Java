66882014fb12911041c496cbefe258f7fd642bbf
HoatFPT-APTECH <92987132+HoatFPT-APTECH@users.noreply.github.com>
2022-05-23 17:55:42 +0700

    Hoat
    Update

--- homeWork_b2/mang/src/main/java/bai_7.java
+++ homeWork_b2/mang/src/main/java/bai_7.java
@@ -1,6 +1,7 @@
 
 import java.util.Scanner;
 import java.util.Arrays;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -11,13 +12,14 @@ import java.util.Arrays;
  * @author hoatd
  */
 public class bai_7 {
-    private  void swap(int arr[],int i,int j){
-        int temp=arr[i];
+    
+    private static void swap(Integer arr[],int i, int j){
+         int temp=arr[i];
         arr[i]=arr[j];
         arr[j]=temp;
     }
     public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("mang co chieu dai n (n<=100) la: ");
         int n = sc.nextInt();
         if (n > 100 || n <= 0) {
@@ -26,19 +28,17 @@ public class bai_7 {
                 n = sc.nextInt();
             }
         }
-        int [] arr = new int[n];
+        Integer [] arr = new Integer[n];
         for (int i = 0; i < n; i++) {
             System.out.println("nhap so nguyen thu " + (i + 1) + " : ");
             arr[i] = sc.nextInt();
         }
-        // phan rieng cua bai 7
-        bai_7 sw= new bai_7();
-        if(n%2==0){
-            for(int i=0;i<n/2;i++){
-               
-                sw.swap(arr,i,(n-i)  );
-            }
-            System.out.println("Mang sau khi sap xep:"+Arrays.toString(arr));
+        //start
+        System.out.println("mang truoc khi dao lon : "+Arrays.toString(arr));
+         int [] arr2= new int [n];
+        for(int j=0;j<n;j++){
+           arr2[j]=arr[n-j];
         }
+        System.out.println("mang sau khi dao lon :"+Arrays.toString(arr2));
     }
 }
