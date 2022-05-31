42dcdbd1524f9410085a00a67a6acbf244feb981
HoatFPT-APTECH <92987132+HoatFPT-APTECH@users.noreply.github.com>
2022-05-31 10:28:27 +0700

    hoat update
    quản lý sinh viên bằng mảng động : ArrayList

--- homeWork_b2/Assingment_1/src/main/java/bai_3.java
+++ homeWork_b2/Assingment_1/src/main/java/bai_3.java
@@ -21,8 +21,8 @@ public class bai_3 {
         if(y%4==0){
             System.out.println("Nam la nam nhuan");
             switch(m){
-                case 4,6,9,11: System.out.println(" Thang co 30 ngay");break;
-                case 1,3,5,7,8,10,12: System.out.println("Thang co 31 ngay");break;
+                case 4,6,9,11 : System.out.println(" Thang co 30 ngay");break;
+                case 1,3,5,7,8,10,12 : System.out.println("Thang co 31 ngay");break;
                 case 2: System.out.println("Thang co 29 ngay");break;
                 default:
                    System.out.println("ban nhap sai thang : thang tu 1->12"); break;
